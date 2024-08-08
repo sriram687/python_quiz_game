bb18d01ca7b7cef45d046653b3e57114e67833ba
pikachu <msram2006@gmail.com>
2024-08-08 12:36:50 +0530

    game was discovered!

--- quiz_game.py
+++ quiz_game.py
@@ -2,13 +2,40 @@ print('Welcome to my computer quiz!')
 
 playing = input("Do you want to play? ")
 
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
 
 print("Okay! let's play:)")
+score = 0
 
 answer = input("What does CPU stands for? ")
-if answer == "central processing unit" :
+if answer.lower() == "central processing unit" :
     print("correct!")
+    score += 1
+
 else:
     print("incorrect! ")
+
+answer = input("What does GPU stands for? ")
+if answer.lower() == "graphics processing unit" :
+    print("correct!")
+    score += 1
+else:
+    print("incorrect! ")
+
+answer = input("What does RAM stands for? ")
+if answer.lower() == "random access memory" :
+    print("correct!")
+    score += 1
+else:
+    print("incorrect! ")
+
+answer = input("What does PSU stands for? ")
+if answer.lower() == "power supply" :
+    print("correct!")
+    score += 1
+else:
+    print("incorrect! ")
+
+print(" you got "     +  str(score)              +     "   correct answer! ")
+print(" you got "     +  str((score / 4) * 100)  +     "   %.  ")
